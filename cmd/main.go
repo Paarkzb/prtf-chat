d2caec8c4a804d43ab120fb35dd2d3a561a94545
Zorikto Baldanov <45837027+Paarkzb@users.noreply.github.com>
2024-08-14 00:21:17 +0800

    handling multiple clients

--- cmd/main.go
+++ cmd/main.go
@@ -5,62 +5,45 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/gorilla/websocket"
+	"chat-server/pkg/websocket"
 )
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-
-	CheckOrigin: func(r *http.Request) bool { return true },
-}
-
-func reader(conn *websocket.Conn) {
-	for {
-		// read
-		messageType, p, err := conn.ReadMessage()
-		if err != nil {
-			log.Panicln(err)
-			return
-		}
-
-		fmt.Println(string(p))
-
-		if err := conn.WriteMessage(messageType, p); err != nil {
-			log.Println(err)
-			return
-		}
-	}
-}
-
 // WS endpoint
-func serveWS(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.Host)
-
+func serveWS(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
+	log.Println("WebSocket Endpoint Hit")
 	// upgrade connection
-	ws, err := upgrader.Upgrade(w, r, nil)
+	conn, err := websocket.Upgrade(w, r)
 	if err != nil {
-		log.Println(err)
+		fmt.Fprintf(w, "%+v\n", err)
 
 	}
 
-	// listen message
-	reader(ws)
+	client := &websocket.Client{
+		Conn: conn,
+		Pool: pool,
+	}
+
+	pool.Register <- client
+	client.Read()
 }
 
 func setupRoutes() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Simple server")
-	})
+	pool := websocket.NewPool()
+	go pool.Start()
 
-	http.HandleFunc("/ws", serveWS)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWS(pool, w, r)
+	})
 }
 
 func main() {
+	log.Println("Chat App v0.01")
+
 	setupRoutes()
 
 	err := http.ListenAndServe(":8071", nil)
 	if err != nil {
 		log.Fatalf("Error: %s", err)
 	}
+	log.Println("Chat App started on port 8071")
 }

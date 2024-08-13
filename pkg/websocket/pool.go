17c6c9be476a5b4853934724a4a072cbe604c33b
Zorikto Baldanov <45837027+Paarkzb@users.noreply.github.com>
2024-08-14 00:24:14 +0800

    little fix

--- pkg/websocket/pool.go
+++ pkg/websocket/pool.go
@@ -24,21 +24,19 @@ func (pool *Pool) Start() {
 		case client := <-pool.Register:
 			pool.Clients[client] = true
 			log.Println("Size of Connection Pool: ", len(pool.Clients))
-			for client, _ := range pool.Clients {
+			for client := range pool.Clients {
 				log.Println(client)
 				client.Conn.WriteJSON(Message{Type: 1, Body: "New User Joined..."})
 			}
-			break
 		case client := <-pool.Unregister:
 			delete(pool.Clients, client)
 			log.Println("Size of Connection Pool: ", len(pool.Clients))
-			for client, _ := range pool.Clients {
+			for client := range pool.Clients {
 				client.Conn.WriteJSON(Message{Type: 1, Body: "User Disconnected..."})
 			}
-			break
 		case message := <-pool.Broadcast:
 			log.Println("Sending message to all clients in Pool")
-			for client, _ := range pool.Clients {
+			for client := range pool.Clients {
 				if err := client.Conn.WriteJSON(message); err != nil {
 					log.Println(err)
 					return
